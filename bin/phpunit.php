2af197fb47b1b82df486664e694debe4dc09cbed
Cydrick Nonog <cydrick.dev@gmail.com>
2023-02-10 23:16:19 +0800

    ci: use scheduler

--- bin/phpunit.php
+++ bin/phpunit.php
@@ -2,10 +2,18 @@
 <?php
 
 
-\Co\run(function () {
+$scheduler = new \Swoole\Coroutine\Scheduler();
+
+$status = 0;
+$scheduler->add(function () use (&$status) {
     global $argc, $argv;
     try {
         require __DIR__ . '/../vendor/bin/phpunit';
     } catch (\Swoole\ExitException $exception) {
+        $status = $exception->getStatus();
     }
 });
+
+$scheduler->start();
+
+exit($status);
